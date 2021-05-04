9a596efba6acf220963f245810124c3c7f303149
ls1925 <103xzy@gmail.com>
2021-05-04 16:26:47 +0800

    list+boxes

--- src/main/java/bag/Test/List.java
+++ src/main/java/bag/Test/List.java
@@ -11,15 +11,16 @@ public class List {
         Integer n = new Integer(13); //Integer n = 13;
         Boolean b = new Boolean(false);
         Character c = new Character('g');
-        Short s = new Short(" s1");
+        Short s = new Short("2");
         Long l = new Long("12345678910");
         String st = new String("string");
         Double d = new Double(12.3654897);
         Float f = new Float(12.2);
+        Byte by = new Byte("8");
         a.add(n);
         a.add(b);
         a.add(c);
-        a.add(s);
+//        a.add(s);
         a.add(l);
         a.add(st);
         a.add(d);
@@ -29,5 +30,14 @@ public class List {
         Box1 box1 = new Box1();
         a.add(box1);
         System.out.println(nums);
+        //Heterogeneous NG!
+        String name = (String)a.get(0);
+        //
+        Box2 box2 = new Box2();
+        ArrayList<Box> boxes = new ArrayList<Box>();
+        boxes.add(box1);
+        boxes.add(box2);
+        Box box = boxes.get(0);
+
     }
 }
